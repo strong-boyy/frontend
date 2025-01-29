ef83ae2af24c19909517496b5454988fbce854a8
DuongHoaiAn2k1 <dhan29112001@gmail.com>
2025-01-29 23:29:32 +0700

    Create ProtectedRoute and RejectedRoute Component for useRouteElement

--- src/useRouteElement.tsx
+++ src/useRouteElement.tsx
@@ -1,4 +1,4 @@
-import { useRoutes } from 'react-router-dom'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
@@ -6,72 +6,96 @@ import RegisterLayout from './layouts/RegisterLayout'
 import MainLayout from './layouts/MainLayout'
 import Setting from './pages/Setting'
 import Task from './pages/Task'
+import { useSelector } from 'react-redux'
+import { RootState } from './store'
+
+function ProtectedRoute() {
+  const isAuthenticated = useSelector((state: RootState) => state.user.isAuthenticated)
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
+
+function RejectedRoute() {
+  const isAuthenticated = useSelector((state: RootState) => state.user.isAuthenticated)
+  return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
+}
 
 export default function useRouteElement() {
   const routeElements = useRoutes([
     {
-      path: '/',
-      element: (
-        <MainLayout>
-          <Home />{' '}
-        </MainLayout>
-      )
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: '/',
+          element: (
+            <MainLayout>
+              <Home />
+            </MainLayout>
+          )
+        },
+        {
+          path: '/time-line',
+          element: (
+            <MainLayout>
+              <Home />
+            </MainLayout>
+          )
+        },
+        {
+          path: '/task',
+          element: (
+            <MainLayout>
+              <Task />
+            </MainLayout>
+          )
+        },
+        {
+          path: '/message',
+          element: (
+            <MainLayout>
+              <Home />
+            </MainLayout>
+          )
+        },
+        {
+          path: '/setting',
+          element: (
+            <MainLayout>
+              <Setting />
+            </MainLayout>
+          )
+        },
+        {
+          path: '/file',
+          element: (
+            <MainLayout>
+              <Home />
+            </MainLayout>
+          )
+        }
+      ]
     },
     {
-      path: '/login',
-      element: (
-        <RegisterLayout>
-          <Login />
-        </RegisterLayout>
-      )
-    },
-    {
-      path: '/register',
-      element: (
-        <RegisterLayout>
-          <Register />
-        </RegisterLayout>
-      )
-    },
-    {
-      path: '/time-line',
-      element: (
-        <MainLayout>
-          <Home />
-        </MainLayout>
-      )
-    },
-    {
-      path: '/task',
-      element: (
-        <MainLayout>
-          <Task />
-        </MainLayout>
-      )
-    },
-    {
-      path: '/message',
-      element: (
-        <MainLayout>
-          <Home />
-        </MainLayout>
-      )
-    },
-    {
-      path: '/setting',
-      element: (
-        <MainLayout>
-          <Setting />
-        </MainLayout>
-      )
-    },
-    {
-      path: '/file',
-      element: (
-        <MainLayout>
-          <Home />
-        </MainLayout>
-      )
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: '/login',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        },
+        {
+          path: '/register',
+          element: (
+            <RegisterLayout>
+              <Register />
+            </RegisterLayout>
+          )
+        }
+      ]
     }
   ])
   return routeElements
